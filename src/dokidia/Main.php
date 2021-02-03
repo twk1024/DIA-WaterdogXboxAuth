dd3642cab46a297d3d6551087d514b8ce194464e
dokidia <42838268+twk1024@users.noreply.github.com>
2021-02-03 22:18:52 +0900

    Added Main source code

--- src/dokidia/Main.php
+++ src/dokidia/Main.php
@@ -1,1 +1,25 @@
+<?php
 
+namespace dokidia;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\Player;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function loginEvent(PlayerLoginEvent $event) {
+        $player = $event->getPlayer();
+        $xuid = $player->getXuid();
+
+        if(!$xuid) {
+            $event->setKickMessage("Please login with Xbox account");
+            $event->setCancelled();
+        }
+    }
+}
